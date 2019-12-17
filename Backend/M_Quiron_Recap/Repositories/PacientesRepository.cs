fce5b322c0b18748062b4ed4a697f9283b287c26
camargogigi <48352841+camargogigi@users.noreply.github.com>
2019-12-17 10:53:58 -0300

    up

---
+++ Backend/M_Quiron_Recap/Repositories/PacientesRepository.cs
@@ -0,0 +1,30 @@
+﻿using M_Quiron_Recap.Domains;
+using M_Quiron_Recap.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M_Quiron_Recap.Repositories
+{
+    public class PacientesRepository : IPacientesRepository
+    {
+        public List<Pacientes> Listar()
+        {
+            using (recapContext ctx = new recapContext())
+            {
+                return ctx.Pacientes.ToList();
+            }
+        }
+
+        public void Cadastrar(Pacientes paciente)
+        {
+            using (recapContext ctx = new recapContext())
+            {
+                ctx.Pacientes.Add(paciente);
+                ctx.SaveChanges();
+            }
+        }
+
+    }
+}
